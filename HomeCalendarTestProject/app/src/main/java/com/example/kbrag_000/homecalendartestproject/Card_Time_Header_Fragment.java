55c0e2eae09b33ef49323a051738b9c5d59935ae
BasedKyle <kbraga111@yahoo.com>
2016-02-10 22:34:14 -0500

    Initial Commit
    First Commit... test

---
+++ HomeCalendarTestProject/app/src/main/java/com/example/kbrag_000/homecalendartestproject/Card_Time_Header_Fragment.java
@@ -0,0 +1,22 @@
+package com.example.kbrag_000.homecalendartestproject;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class Card_Time_Header_Fragment extends Fragment {
+
+    public Card_Time_Header_Fragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_card__time__header, container, false);
+    }
+}
---
+++ HomeCalendarTestProject/app/src/main/java/com/example/kbrag_000/homecalendartestproject/Home_ScheduleFragment.java
@@ -0,0 +1,22 @@
+package com.example.kbrag_000.homecalendartestproject;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class Home_ScheduleFragment extends Fragment {
+
+    public Home_ScheduleFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home__schedule, container, false);
+    }
+}
