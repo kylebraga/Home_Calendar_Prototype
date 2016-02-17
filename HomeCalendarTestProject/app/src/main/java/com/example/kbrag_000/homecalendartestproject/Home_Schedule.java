0f7a6a84552f6c9c778b74a206d3b4bc2b1eca2d
BasedKyle <kbraga111@yahoo.com>
2016-02-16 19:55:43 -0500

    Basic App Bar
    simple app bar implementation

--- HomeCalendarTestProject/app/src/main/java/com/example/kbrag_000/homecalendartestproject/Home_Schedule.java
+++ HomeCalendarTestProject/app/src/main/java/com/example/kbrag_000/homecalendartestproject/Home_Schedule.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -60,6 +61,8 @@ public class Home_Schedule extends AppCompatActivity {
 
         mAdapter = new CalendarRecyclerAdapter(myDataset);
         mRecyclerView.setAdapter(mAdapter);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         DrawerLayout Drawer;
     }
