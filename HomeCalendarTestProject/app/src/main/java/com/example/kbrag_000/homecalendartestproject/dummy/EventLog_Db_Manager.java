3a312aef9e97eea3f0935bbaef2a2733154b5a62
BasedKyle <kbraga111@yahoo.com>
2016-02-28 21:49:44 -0500

    Insert and Get methods
    database quieries first attempt

--- HomeCalendarTestProject/app/src/main/java/com/example/kbrag_000/homecalendartestproject/dummy/EventLog_Db_Manager.java
+++ HomeCalendarTestProject/app/src/main/java/com/example/kbrag_000/homecalendartestproject/dummy/EventLog_Db_Manager.java
@@ -1,10 +1,8 @@
 package com.example.kbrag_000.homecalendartestproject.dummy;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-
-
-
 
 /**
  * Created by basedkyle on 2/28/16.
@@ -17,22 +15,24 @@ public class EventLog_Db_Manager {
     public String mEventNameLabel = "event_name";
     public String mEventOwnerLabel = "owner_name";
     public String mEventStartLabel = "start_time";
-    public String mEventEndlabel = "end_time";
+    public String mEventEndLabel  = "end_time";
+    public String mEventDateLabel = "event_date";
     public String mEventColorLabel = "event_color";
     public String mEntryType = " VARCHAR";
     public String mCommaSep = ",";
     public SQLiteDatabase eventDb;
 
+    public String mCurrentEventId;
+    public String mCurrentEventName;
+    public String mCurrentEventOwner;
+    public String mCurrentventStart;
+    public String mCurrentEventEnd;
+    public String mCurrentDate;
+    public String mCurrentEventColor;
+
     EventLog_Db_Manager(String tableInput)
     {
-
-    }
-
-    public void getAllEventsForDay()
-    {
-
-
-
+        CreateEventLog();
     }
 
     public void CreateEventLog()
@@ -41,14 +41,67 @@ public class EventLog_Db_Manager {
 
         eventDb.execSQL("CREATE TABLE IF NOT EXISTS" +
                 mTableName + "(" +
-                                    mEventIdLabel + mEntryType + mCommaSep +
-                                    mEventNameLabel + mEntryType + mCommaSep +
-                                    mEventIdLabel + mEntryType + mCommaSep +
-                                    mEventOwnerLabel + mEntryType + mCommaSep +
-                                    mEventStartLabel + mEntryType + mCommaSep +
-                                    mEventEndlabel + mEntryType + mCommaSep +
-                                    mEventColorLabel + mEntryType + mCommaSep +
+                                mEventIdLabel + mEntryType + mCommaSep +
+                                mEventNameLabel + mEntryType + mCommaSep +
+                                mEventIdLabel + mEntryType + mCommaSep +
+                                mEventOwnerLabel + mEntryType + mCommaSep +
+                                mEventStartLabel + mEntryType + mCommaSep +
+                                mEventEndLabel  + mEntryType + mCommaSep +
+                                mEventDateLabel  + mEntryType + mCommaSep +
+                                mEventColorLabel + mEntryType + mCommaSep +
                             ");");
     }
 
+    public void CallInsertToEventLog(String idInput, String nameInput, String ownerInput,
+                                     String startInput, String endInput, String colorInput, String dateInput)
+    {
+        mCurrentEventId = idInput;
+        mCurrentEventName = nameInput;
+        mCurrentEventOwner = ownerInput ;
+        mCurrentventStart = startInput;
+        mCurrentEventEnd = endInput;
+        mCurrentDate = dateInput;
+        mCurrentEventColor = colorInput;
+
+        InsertToEventLog();
+    }
+    private void InsertToEventLog()
+    {
+        eventDb.execSQL("INSERT INTO" + mTableName + " VALUES(" +
+                mCurrentEventId + mCommaSep +
+                mCurrentEventName + mCommaSep +
+                mCurrentEventOwner + mCommaSep +
+                mCurrentventStart + mCommaSep +
+                mCurrentEventEnd + mCommaSep +
+                mCurrentEventColor + mCommaSep +
+                ");");
+    }
+    public void CallGetFromEventLog(String dateInput)
+    {
+        mCurrentDate = dateInput;
+        GetFromEventLog();
+    }
+    private void GetFromEventLog()
+    {
+        String queryString = "SELECT *" +
+                            " FROM " + mTableName +
+                            " WHERE " + mEventDateLabel + "= \"" + mCurrentDate + "\"";
+
+        Cursor c = eventDb.rawQuery(queryString, null);
+
+        if(c.getCount() == 0)
+        {
+            return;
+        }
+
+        while(c.moveToNext()) {
+            mCurrentEventId = c.getString(0);
+            mCurrentEventName = c.getString(1);
+            mCurrentEventOwner = c.getString(2);
+            mCurrentventStart = c.getString(3);
+            mCurrentEventEnd = c.getString(4);
+            mCurrentDate = c.getString(5);
+            mCurrentEventColor = c.getString(6);
+        }
+    }
 }
